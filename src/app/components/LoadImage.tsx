1155d5523d294080e1569a7468820145e3bc1f9b
sam <wong.sie.ung@qq.com>
2023-07-18 12:22:23 +0800

    Hi

---
+++ src/app/components/LoadImage.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+
+interface Props {
+  imageSrc: string
+  domainName: string
+  domainAddress: string
+}
+
+const ImageComponent = (props: Props) => {
+  const [loadingTime, setLoadingTime] = useState(0)
+  const ran = Math.random()
+  const img = props.imageSrc + '?' + ran
+
+  useEffect(() => {
+    const image = new Image()
+    const startTime = performance.now()
+
+    image.onload = () => {
+      const endTime = performance.now()
+
+      const timeInSeconds = Math.floor(endTime - startTime)
+      setLoadingTime(timeInSeconds)
+    }
+
+    image.src = img
+
+    // Cleanup
+    return () => {
+      image.onload = null
+    }
+  }, [])
+
+  return (
+    <Link href={props.domainAddress}>
+      <div className='rounded-lg p-10 border-solid border-2 border-yellow-500'>
+        {/* <img src={props.imageSrc} /> */}
+        <p></p>
+        {loadingTime !== null && <p>{loadingTime} 纳秒</p>}
+        <div>{props.domainName}</div>
+      </div>
+    </Link>
+  )
+}
+
+export default ImageComponent
